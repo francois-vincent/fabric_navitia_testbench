8aaef23cccff7cf232e28c29ae56f9ba4a520720
francois-vincent <francois.vincent01@gmail.com>
2016-05-26 18:13:58 +0200

    add some tests, kraken tests refactoring

--- test_kraken/test_duplicated.py
+++ test_kraken/test_duplicated.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-import pytest
+import time
 
 from ..test_common import skipifdev
 from ..test_common.test_kraken import (_test_stop_restart_kraken,
@@ -11,19 +11,22 @@ from ..test_common.test_kraken import (_test_stop_restart_kraken,
 from ..utils import get_running_krakens
 
 
-def test_kraken_setup(duplicated):
-    duplicated, fabric = duplicated
-    for krak in fabric.env.instances:
-        assert duplicated.path_exists('/etc/init.d/kraken_{}'.format(krak))
-        assert duplicated.path_exists('/srv/kraken/{}/kraken.ini'.format(krak))
-        assert duplicated.path_exists('/etc/jormungandr.d/{}.json'.format(krak), 'host1')
-        assert duplicated.path_exists('/etc/jormungandr.d/{}.json'.format(krak), 'host2', negate=True)
-
-
+SHOW_CALL_TRACKER_DATA = True
 nominal_krakens = {'host1': {'us-wa', 'fr-nw', 'fr-npdc', 'fr-ne-amiens', 'fr-idf', 'fr-cen'},
                    'host2': {'us-wa', 'fr-nw', 'fr-npdc', 'fr-ne-amiens', 'fr-idf', 'fr-cen'}}
 krakens_after_stop = {'host1': {'fr-nw', 'fr-npdc', 'fr-idf', 'fr-cen'},
                       'host2': {'fr-nw', 'fr-npdc', 'fr-idf', 'fr-cen'}}
+
+
+@skipifdev
+def test_kraken_setup(duplicated):
+    platform, fabric = duplicated
+    for krak in fabric.env.instances:
+        assert platform.path_exists('/etc/init.d/kraken_{}'.format(krak))
+        assert platform.path_exists('/srv/kraken/{}/kraken.ini'.format(krak))
+        assert platform.path_exists('/etc/jormungandr.d/{}.json'.format(krak), 'host1')
+        assert platform.path_exists('/etc/jormungandr.d/{}.json'.format(krak), 'host2', negate=True)
+        assert platform.path_exists('/var/log/kraken/{}.log'.format(krak))
 
 
 @skipifdev
@@ -73,6 +76,8 @@ def test_stop_start_apache(duplicated):
 
 @skipifdev
 def test_test_kraken_nowait_nofail(duplicated, capsys):
+    # wait for krakens to be fully started
+    time.sleep(15)
     _test_test_kraken_nowait_nofail(duplicated, capsys,
                                     map={'host1': {'us-wa'}, 'host2': {'fr-ne-amiens'}}, ret_val=None)
 
@@ -88,44 +93,90 @@ def test_get_no_data_instances(duplicated, capsys):
 
 
 @skipifdev
-def test_test_all_krakens_no_wait(duplicated, capsys):
+def test_test_all_krakens_no_wait(duplicated):
     platform, fabric = duplicated
-    fabric.execute('test_all_krakens')
-    out, err = capsys.readouterr()
+    time.sleep(15)
+    value, exception, stdout, stderr = fabric.execute_forked('test_all_krakens')
+    assert stdout.count('WARNING: ') == 2 * len(fabric.env.instances)
     for instance in fabric.env.instances:
-        assert "WARNING: instance {} has no loaded data".format(instance) in out
-
-
-# @skipifdev
-# This test is temporarily removed because I don't know yet how to restart fabric connections
-# that are closed when an exception is raised below
-# def test_check_dead_instances(duplicated, capsys):
-#     platform, fabric = duplicated
-#     with pytest.raises(SystemExit):
-#         fabric.execute('component.kraken.check_dead_instances')
-#     out, err = capsys.readouterr()
-#     assert 'The threshold of allowed dead instances is exceeded: ' \
-#            'Found 12 dead instances out of 6.' in out
+        assert stdout.count("WARNING: instance {} has no loaded data".format(instance)) == 2
 
 
 @skipifdev
-def test_create_remove_eng_instance(duplicated, capsys):
+def test_check_dead_instances(duplicated):
+    platform, fabric = duplicated
+    value, exception, stdout, stderr = fabric.execute_forked('component.kraken.check_dead_instances')
+    assert value is None
+    assert isinstance(exception, SystemExit)
+    assert 'The threshold of allowed dead instances is exceeded: ' \
+           'Found 12 dead instances out of 6.' in stdout
+
+
+@skipifdev
+def test_create_remove_eng_instance(duplicated):
     platform, fabric = duplicated
     fabric.get_object('instance.add_instance')('toto', 'passwd',
                        zmq_socket_port=30004, zmq_server=(fabric.env.host1_ip, fabric.env.host2_ip))
-    fabric.execute('create_eng_instance', 'toto')
-    out, err = capsys.readouterr()
-    try:
-        assert 'INFO: kraken toto instance is running on {}'.format(platform.get_hosts()['host1']) in out
-        assert platform.path_exists('/srv/kraken/toto/kraken.ini')
-        assert platform.path_exists('/etc/init.d//kraken_toto')
-        assert platform.path_exists('/var/log/kraken/toto.log')
-        assert set(get_running_krakens(platform, 'host1')) == {'toto'} | nominal_krakens['host1']
-        assert set(get_running_krakens(platform, 'host2')) == {'toto'} | nominal_krakens['host2']
-    finally:
-        fabric.execute('remove_kraken_instance', 'toto', purge_logs=True)
+
+    with fabric.set_call_tracker('component.kraken.update_eng_instance_conf') as data:
+        value, exception, stdout, stderr = fabric.execute_forked('create_eng_instance', 'toto')
+
+    if SHOW_CALL_TRACKER_DATA:
+        from pprint import pprint
+        pprint(dict(data()))
+    # there is only one call to update_eng_instance_conf
+    assert len(data()['update_eng_instance_conf']) == 2
+    host1_string = 'root@{}'.format(platform.get_hosts()['host1'])
+    host2_string = 'root@{}'.format(platform.get_hosts()['host2'])
+    # first parameter is the newly created instance
+    assert data()['update_eng_instance_conf'][0][0][0].name == 'toto'
+    assert data()['update_eng_instance_conf'][1][0][0].name == 'toto'
+    # second parameter is the host string
+    assert set(x[0][1]for x in data()['update_eng_instance_conf']) == {host1_string, host2_string}
+
+    time.sleep(2)
+    assert 'INFO: kraken toto instance is running on {}'.format(platform.get_hosts()['host1']) in stdout
+    assert 'INFO: kraken toto instance is running on {}'.format(platform.get_hosts()['host2']) in stdout
+    assert platform.path_exists('/srv/kraken/toto/kraken.ini')
+    assert platform.path_exists('/etc/init.d//kraken_toto')
+    assert platform.path_exists('/var/log/kraken/toto.log')
+    # check that new kraken is running on both hosts
+    assert set(get_running_krakens(platform, 'host1')) == {'toto'} | nominal_krakens['host1']
+    assert set(get_running_krakens(platform, 'host2')) == {'toto'} | nominal_krakens['host2']
+
+    fabric.execute('remove_kraken_instance', 'toto', purge_logs=True)
     assert platform.path_exists('/srv/kraken/toto/kraken.ini', negate=True)
     assert platform.path_exists('/etc/init.d//kraken_toto', negate=True)
     assert platform.path_exists('/var/log/kraken/toto.log', negate=True)
     assert set(get_running_krakens(platform, 'host1')) == nominal_krakens['host1']
     assert set(get_running_krakens(platform, 'host2')) == nominal_krakens['host2']
+
+
+# @skipifdev
+def test_restart_all_krakens(duplicated):
+    platform, fabric = duplicated
+
+    fabric.env.excluded_instances = ['us-wa', 'fr-nw']
+    with fabric.set_call_tracker('component.kraken.require_monitor_kraken_started',
+                                 'component.kraken.restart_kraken_on_host') as data:
+        value, exception, stdout, stderr = fabric.execute_forked('component.kraken.restart_all_krakens', wait=False)
+
+    if SHOW_CALL_TRACKER_DATA:
+        from pprint import pprint
+        pprint(dict(data()))
+    assert len(data()['require_monitor_kraken_started']) == 2
+    # call to restart_kraken is not dependant on env.excluded_instances
+    # every kraken must be restarted
+    assert len(data()['restart_kraken_on_host']) == 12
+
+    host1 = platform.get_hosts()['host1']
+    host2 = platform.get_hosts()['host2']
+    for instance in fabric.env.excluded_instances:
+        assert stdout.count("{} has no data, not testing it".format(instance)) == 1
+    for instance in set(fabric.env.instances).difference(fabric.env.excluded_instances):
+        assert stdout.count('calling : http://{}:80/monitor-kraken/?instance={}'
+                            .format(host1, instance)) == 1
+        assert stdout.count('calling : http://{}:80/monitor-kraken/?instance={}'
+                            .format(host2, instance)) == 1
+        assert stdout.count('OK: instance {} has correct values:'.format(instance)) == 2
+
--- test_tasks/test_duplicated.py
+++ test_tasks/test_duplicated.py
@@ -5,6 +5,7 @@ import time
 from ..test_common import skipifdev
 
 
+SHOW_CALL_TRACKER_DATA = True
 instances_names = {'us-wa', 'fr-nw', 'fr-npdc', 'fr-ne-amiens', 'fr-idf', 'fr-cen'}
 
 
@@ -19,6 +20,9 @@ def test_upgrade_kraken(duplicated):
         fabric.execute_forked(
             'tasks.upgrade_kraken', kraken_wait=False, up_confs=False, supervision=False)
 
+    if SHOW_CALL_TRACKER_DATA:
+        from pprint import pprint
+        pprint(dict(data()))
     assert len(data()['upgrade_engine_packages']) == 2
     assert len(data()['upgrade_monitor_kraken_packages']) == 2
     assert len(data()['require_monitor_kraken_started']) == 2
@@ -40,6 +44,9 @@ def test_upgrade_kraken_restricted(duplicated):
         fabric.execute_forked(
             'tasks.upgrade_kraken', kraken_wait=False, up_confs=False, supervision=False)
 
+    if SHOW_CALL_TRACKER_DATA:
+        from pprint import pprint
+        pprint(dict(data()))
     # upgrades apply only on restricted pool
     assert len(data()['upgrade_engine_packages']) == 1
     assert len(data()['upgrade_monitor_kraken_packages']) == 1
@@ -50,13 +57,14 @@ def test_upgrade_kraken_restricted(duplicated):
     assert set((x[0][0].name for x in data()['restart_kraken_on_host'])) == instances_names
 
 
-# @skipifdev
+@skipifdev
 def test_upgrade_all_load_balancer(duplicated):
     platform, fabric = duplicated
     fabric.env.use_load_balancer = True
     # postgres is really long to warm up !
     time.sleep(15)
 
+    # most of this test is unitary test: detailed functions are not called, they are traced
     with fabric.set_call_tracker('-tasks.check_last_dataset',
                                  '-tasks.upgrade_tyr',
                                  '-tasks.upgrade_kraken',
@@ -73,8 +81,26 @@ def test_upgrade_all_load_balancer(duplicated):
     assert stderr == ''
     assert stdout.count("Executing task 'stop_tyr_beat'") == 1
     assert stdout.count("Executing task 'start_tyr_beat'") == 1
+    if SHOW_CALL_TRACKER_DATA:
+        from pprint import pprint
+        pprint(dict(data()))
+    # 1 call to component.load_balancer.disable_node by reload_jormun_safe()
+    assert len(data()['disable_node']) == 1
+    # 1 call to component.load_balancer.enable_node by reload_jormun_safe()
+    assert len(data()['enable_node']) == 1
+    # 4 calls: 1 eng_hosts_1, 1 ws_hosts_1, 1 eng_hosts_2, 1 empty
     assert len(data()['disable_nodes']) == 4
+    for i, x in enumerate((fabric.env.eng_hosts_1, fabric.env.ws_hosts_1, fabric.env.eng_hosts_2, [])):
+        assert data()['disable_nodes'][i][0][0] == x
+    # 3 calls: 1 eng_hosts_1, 1 ws_hosts_1, 1 eng_hosts_1
     assert len(data()['enable_nodes']) == 3
-    assert len(data()['upgrade_jormungandr']) == 2
+    for i, x in enumerate((fabric.env.eng_hosts_1, fabric.env.ws_hosts_1, fabric.env.eng_hosts)):
+        assert data()['enable_nodes'][i][0][0] == x
+    # 1 call in first phase with supervision, 1 call in second phase without supervision
     assert len(data()['upgrade_kraken']) == 2
+    assert data()['upgrade_kraken'][0][1].get('supervision') is True
+    assert data()['upgrade_kraken'][1][1].get('supervision') is None
+    # 1 call in first phase, 1 call in second phase
+    assert len(data()['upgrade_jormungandr']) == 2
+    # only one phase
     assert len(data()['upgrade_tyr']) == 1
